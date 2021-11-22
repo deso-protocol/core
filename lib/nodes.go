ff22bc976084b22034522ee32af12dbd6335dc68
maebeam <maebeam0@gmail.com>
2021-11-22 13:50:09 -0500

    Update nodes.go

--- lib/nodes.go
+++ lib/nodes.go
@@ -1,8 +1,13 @@
 package lib
 
 type DeSoNode struct {
+	// Name of the node, displayed to users
 	Name  string
+	
+	// HTTPs URL to the node or app
 	URL   string
+	
+	// DeSo username of the node owner
 	Owner string
 }
 
