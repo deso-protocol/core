62ea3e6a2bd67ed67b6f5766c2ce2b1ef074d6f1
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2022-02-24 16:11:35 -0500

    add remaining UtxoTypes to UtxoType.String() (#229)

--- lib/block_view_types.go
+++ lib/block_view_types.go
@@ -34,17 +34,30 @@ const (
 )
 
 func (mm UtxoType) String() string {
-	if mm == UtxoTypeOutput {
+	switch mm {
+	case UtxoTypeOutput:
 		return "UtxoTypeOutput"
-	} else if mm == UtxoTypeBlockReward {
+	case UtxoTypeBlockReward:
 		return "UtxoTypeBlockReward"
-	} else if mm == UtxoTypeBitcoinBurn {
+	case UtxoTypeBitcoinBurn:
 		return "UtxoTypeBitcoinBurn"
-	} else if mm == UtxoTypeStakeReward {
+	case UtxoTypeStakeReward:
 		return "UtxoTypeStakeReward"
+	case UtxoTypeCreatorCoinSale:
+		return "UtxoTypeCreatorCoinSale"
+	case UtxoTypeCreatorCoinFounderReward:
+		return "UtxoTypeCreatorCoinFounderReward"
+	case UtxoTypeNFTSeller:
+		return "UtxoTypeNFTSeller"
+	case UtxoTypeNFTBidderChange:
+		return "UtxoTypeNFTBidderChange"
+	case UtxoTypeNFTCreatorRoyalty:
+		return "UtxoTypeNFTCreatorRoyalty"
+	case UtxoTypeNFTAdditionalDESORoyalty:
+		return "UtxoTypeNFTAdditionalDESORoyalty"
+	default:
+		return "UtxoTypeUnknown"
 	}
-
-	return "UtxoTypeUnknown"
 }
 
 // UtxoEntry identifies the data associated with a UTXO.
