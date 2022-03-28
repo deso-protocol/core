35d5081c591eb452fa09a28a7b62d49bb9791b9f
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2022-03-28 12:12:16 -0400

    Add log

--- lib/peer.go
+++ lib/peer.go
@@ -1173,6 +1173,7 @@ func (pp *Peer) IsSyncCandidate() bool {
 	// TODO: This is a bit of a messy way to determine whether the node was
 	// run with --hypersync
 	nodeSupportsHypersync := (pp.serviceFlags & SFHyperSync) != 0
+	glog.Infof("IsSyncCandidate: cmgr (%v), nodeSupportsHyperSync (%v)", pp.cmgr, nodeSupportsHypersync)
 	hypersyncSatisfied := !pp.cmgr.DisableSlowSync || nodeSupportsHypersync
 	glog.Infof("IsSyncCandidate: localAddr (%v), isFullNode (%v), "+
 		"hypersyncSatisfied (%v), --disableSlowSync (%v), nodeSupportsHypersync (%v), "+
