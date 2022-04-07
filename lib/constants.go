322b34d509fbbbbf141532dea6029bfdfd24c51d
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2022-04-06 22:15:26 -0400

    Increase chunk size

--- lib/constants.go
+++ lib/constants.go
@@ -57,7 +57,7 @@ const (
 	SnapshotBlockHeightPeriod uint64 = 1000
 
 	// SnapshotBatchSize is the size in bytes of the snapshot batches sent to peers
-	SnapshotBatchSize uint32 = 8 << 20 // 8MB
+	SnapshotBatchSize uint32 = 100 << 20 // 100MB
 
 	// DatabaseCacheSize is used to save read operations when fetching records from the main Db.
 	DatabaseCacheSize uint = 1000000 // 1M
