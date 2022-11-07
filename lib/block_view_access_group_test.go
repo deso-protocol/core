7c20e6ed550d2e1a9c983e50f8a5bf1f3d31e134
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2022-11-07 08:23:57 -0800

    add some tests

---
+++ lib/block_view_access_group_test.go
@@ -0,0 +1,62 @@
+package lib
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAccessGroupCreate(t *testing.T) {
+	require := require.New(t)
+	_ = require
+
+	chain, params, db := NewLowDifficultyBlockchain()
+	mempool, miner := NewTestMiner(t, chain, params, true /*isSender*/)
+
+	// Mine a few blocks to give the senderPkString some money.
+	_, err := miner.MineAndProcessSingleBlock(0 /*threadIndex*/, mempool)
+	require.NoError(err)
+	_, err = miner.MineAndProcessSingleBlock(0 /*threadIndex*/, mempool)
+	require.NoError(err)
+	_, err = miner.MineAndProcessSingleBlock(0 /*threadIndex*/, mempool)
+	require.NoError(err)
+	_, err = miner.MineAndProcessSingleBlock(0 /*threadIndex*/, mempool)
+	require.NoError(err)
+
+	// Setup some convenience functions for the test.
+	txnOps := [][]*UtxoOperation{}
+	txns := []*MsgDeSoTxn{}
+	expectedSenderBalances := []uint64{}
+	expectedRecipientBalances := []uint64{}
+
+	// We take the block tip to be the blockchain height rather than the
+	// header chain height.
+	savedHeight := chain.blockTip().Height + 1
+	registerOrTransfer := func(username string,
+		senderPk string, recipientPk string, senderPriv string) {
+
+		expectedSenderBalances = append(expectedSenderBalances, _getBalance(t, chain, nil, senderPkString))
+		expectedRecipientBalances = append(expectedRecipientBalances, _getBalance(t, chain, nil, recipientPkString))
+
+		currentOps, currentTxn, _ := _doBasicTransferWithViewFlush(
+			t, chain, db, params, senderPk, recipientPk,
+			senderPriv, 7 /*amount to send*/, 11 /*feerate*/)
+
+		txnOps = append(txnOps, currentOps)
+		txns = append(txns, currentTxn)
+	}
+
+	// Fund all the keys.
+	registerOrTransfer("", senderPkString, m0Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m1Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m1Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m1Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m1Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m1Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m1Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m2Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m2Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m3Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m3Pub, senderPrivString)
+	registerOrTransfer("", senderPkString, m3Pub, senderPrivString)
+
+}
