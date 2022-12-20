f098ee3edfa75a16347adf9c3ca54b4ed28d34b8
mattfoley8 <isaac@deso.org>
2022-12-20 16:53:23 -0500

    Update comments more.

--- lib/block_view_association.go
+++ lib/block_view_association.go
@@ -777,14 +777,14 @@ func (bav *UtxoView) GetUserAssociationByAttributes(transactorPK []byte, metadat
 			continue
 		}
 		if utxoViewAssociationEntry.isDeleted {
-			// If there is a deleted matching association entry in the UTXO view, we
-			// need to keep searching the UTXO view since there could be other
-			// non-deleted matches. There can be an arbitrary number of deleted
-			// matching association entries, but there will only ever be zero or one
-			// non-deleted matching association entry, since "updating" a matching
-			// association entry deletes the old one and creates a new one. If we
-			// don't find any matches in the UTXO view, we return nil below, before
-			// checking the db which would return a deleted association entry.
+			// If there is a deleted matching association entry in the UTXO view, we need
+			// to keep searching the UTXO view since there could be other non-deleted
+			// matches. There can be an arbitrary number of deleted matching association
+			// entries, but there will only ever be zero or one non-deleted matching
+			// association entry, since "updating" a matching association entry deletes
+			// the old one and creates a new one with a new association ID. If we don't
+			// find any matches in the UTXO view, we return nil below, before checking
+			// the db which would return a deleted association entry.
 			isDeleted = true
 			continue
 		}
@@ -815,14 +815,14 @@ func (bav *UtxoView) GetPostAssociationByAttributes(transactorPK []byte, metadat
 			continue
 		}
 		if utxoViewAssociationEntry.isDeleted {
-			// If there is a deleted matching association entry in the UTXO view, we
-			// need to keep searching the UTXO view since there could be other
-			// non-deleted matches. There can be an arbitrary number of deleted
-			// matching association entries, but there will only ever be zero or one
-			// non-deleted matching association entry, since "updating" a matching
-			// association entry deletes the old one and creates a new one. If we
-			// don't find any matches in the UTXO view, we return nil below, before
-			// checking the db which would return a deleted association entry.
+			// If there is a deleted matching association entry in the UTXO view, we need
+			// to keep searching the UTXO view since there could be other non-deleted
+			// matches. There can be an arbitrary number of deleted matching association
+			// entries, but there will only ever be zero or one non-deleted matching
+			// association entry, since "updating" a matching association entry deletes
+			// the old one and creates a new one with a new association ID. If we don't
+			// find any matches in the UTXO view, we return nil below, before checking
+			// the db which would return a deleted association entry.
 			isDeleted = true
 			continue
 		}
