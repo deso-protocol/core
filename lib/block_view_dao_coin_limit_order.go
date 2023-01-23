7170854048b0f289bbd7c580bc878daaa813b55f
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2023-01-23 18:46:12 -0500

    Add isDeleted check when canceling order (#363)
    * Add isDeleted check when canceling order
    
    * Add block heights to the cancel DAO Coin Limit Order Fix
    
    * rename new fork height to match standard
    
    * gofmt
    
    * update fork height name
    
    Co-authored-by: Lazy Nina <>

--- lib/block_view_dao_coin_limit_order.go
+++ lib/block_view_dao_coin_limit_order.go
@@ -293,7 +293,7 @@ func (bav *UtxoView) _connectDAOCoinLimitOrder(
 		if err != nil {
 			return 0, 0, nil, err
 		}
-		if existingTransactorOrder == nil {
+		if existingTransactorOrder == nil || (blockHeight >= bav.Params.ForkHeights.AssociationsAndAccessGroupsBlockHeight && existingTransactorOrder.isDeleted) {
 			return 0, 0, nil, RuleErrorDAOCoinLimitOrderToCancelNotFound
 		}
 		if !transactorPKIDEntry.PKID.Eq(existingTransactorOrder.TransactorPKID) {
