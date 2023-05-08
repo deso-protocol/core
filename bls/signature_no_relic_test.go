54d46bc50480b6b8c7be91e77b0a92ec7d999af3
mattfoley8 <isaac@deso.org>
2023-05-08 15:18:00 -0400

    Expose NewPrivateKey().

--- bls/signature_no_relic_test.go
+++ bls/signature_no_relic_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestVerifyingBLSSignatures(t *testing.T) {
+	assert.Panics(t, func() { NewPrivateKey() })
 	assert.Panics(t, func() { (&PrivateKey{}).FromString("") })
 	assert.Panics(t, func() { (&PublicKey{}).FromString("") })
 	assert.Panics(t, func() { (&Signature{}).FromString("") })
