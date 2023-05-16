148f78aef7aad24772e5e2d5ddd91ef29449f4bc
lazynina <lazynina84@gmail.com>
2023-05-15 21:00:35 -0400

    always flush utxos to badger as well

--- lib/block_view_flush.go
+++ lib/block_view_flush.go
@@ -51,9 +51,6 @@ func (bav *UtxoView) FlushToDbWithTxn(txn *badger.Txn, blockHeight uint64) error
 
 	// Only flush to BadgerDB if Postgres is disabled
 	if bav.Postgres == nil {
-		if err := bav._flushUtxosToDbWithTxn(txn, blockHeight); err != nil {
-			return err
-		}
 		if err := bav._flushProfileEntriesToDbWithTxn(txn, blockHeight); err != nil {
 			return err
 		}
@@ -135,6 +132,9 @@ func (bav *UtxoView) FlushToDbWithTxn(txn *badger.Txn, blockHeight uint64) error
 		return err
 	}
 	if err := bav._flushNonceEntriesToDbWithTxn(txn); err != nil {
+		return err
+	}
+	if err := bav._flushUtxosToDbWithTxn(txn, blockHeight); err != nil {
 		return err
 	}
 	if err := bav._flushDeSoBalancesToDbWithTxn(txn); err != nil {
