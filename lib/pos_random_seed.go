fcdd168d377574645b88174312befe3ab514abc9
mattfoley8 <isaac@deso.org>
2023-05-19 12:06:19 -0400

    Add ToUint256() method.

--- lib/pos_random_seed.go
+++ lib/pos_random_seed.go
@@ -6,11 +6,16 @@ import (
 	"github.com/deso-protocol/core/bls"
 	"github.com/dgraph-io/badger/v3"
 	"github.com/golang/glog"
+	"github.com/holiman/uint256"
 	"github.com/pkg/errors"
 	"io"
 )
 
 type RandomSeedHash [32]byte
+
+func (randomSeedHash *RandomSeedHash) ToUint256() *uint256.Int {
+	return uint256.NewInt().SetBytes(randomSeedHash.ToBytes())
+}
 
 func (randomSeedHash *RandomSeedHash) Copy() *RandomSeedHash {
 	randomSeedHashCopy := &RandomSeedHash{}
