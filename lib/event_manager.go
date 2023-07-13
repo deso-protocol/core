4c1b7f4167fe99161dbb097cae82e736549a8121
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2023-07-13 16:54:52 -0700

    Revert "Split Database"
    This reverts commit 07e9f4d0119ed13692793eddd439dcf43d4fd64f.

--- lib/event_manager.go
+++ lib/event_manager.go
@@ -3,6 +3,7 @@ package lib
 type TransactionEventFunc func(event *TransactionEvent)
 type BlockEventFunc func(event *BlockEvent)
 type SnapshotCompletedEventFunc func()
+type MempoolEventFunc func(event *MempoolEvent)
 
 type TransactionEvent struct {
 	Txn     *MsgDeSoTxn
@@ -21,12 +22,18 @@ type BlockEvent struct {
 	UtxoOps  [][]*UtxoOperation
 }
 
+type MempoolEvent struct {
+	Txn  *MempoolTx
+	Type MempoolEventType
+}
+
 type EventManager struct {
 	transactionConnectedHandlers []TransactionEventFunc
 	blockConnectedHandlers       []BlockEventFunc
 	blockDisconnectedHandlers    []BlockEventFunc
 	blockAcceptedHandlers        []BlockEventFunc
 	snapshotCompletedHandlers    []SnapshotCompletedEventFunc
+	mempoolTransactionHandlers   []MempoolEventFunc
 }
 
 func NewEventManager() *EventManager {
@@ -79,6 +86,16 @@ func (em *EventManager) OnBlockAccepted(handler BlockEventFunc) {
 
 func (em *EventManager) blockAccepted(event *BlockEvent) {
 	for _, handler := range em.blockAcceptedHandlers {
+		handler(event)
+	}
+}
+
+func (em *EventManager) OnMempoolEvent(handler MempoolEventFunc) {
+	em.mempoolTransactionHandlers = append(em.mempoolTransactionHandlers, handler)
+}
+
+func (em *EventManager) mempoolEvent(event *MempoolEvent) {
+	for _, handler := range em.mempoolTransactionHandlers {
 		handler(event)
 	}
 }
