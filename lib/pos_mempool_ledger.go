56e1d9e8d000b4721d3c06254c43eba0c040d14e
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2023-07-16 20:35:49 -0700

    Renames; Function signature changes

--- lib/pos_mempool_ledger.go
+++ lib/pos_mempool_ledger.go
@@ -6,34 +6,34 @@ import (
 	"sync"
 )
 
-type PosMempoolLedger struct {
+// BalanceLedger is a simple in-memory ledger of reserved balances for user public keys. The entries in the ledger can be
+// increased or decreased, as long as user's reserved balance doesn't exceed the user's spendable balance. It allows for checking
+// whether a balance can be safely increased without going over the user's spendable balance. It also allows for
+// increasing and decreasing balances.
+type BalanceLedger struct {
 	sync.RWMutex
 
-	reservedBalances map[PublicKey]uint64
+	reservedBalancesNanos map[PublicKey]uint64
 }
 
-func NewPosMempoolLedger() *PosMempoolLedger {
-	return &PosMempoolLedger{
-		reservedBalances: make(map[PublicKey]uint64),
+func NewPosMempoolLedger() *BalanceLedger {
+	return &BalanceLedger{
+		reservedBalancesNanos: make(map[PublicKey]uint64),
 	}
 }
 
-func (pml *PosMempoolLedger) CheckBalanceIncrease(publicKey PublicKey, amount uint64, blockView *UtxoView, blockHeight uint32) error {
+func (pml *BalanceLedger) CheckBalanceIncrease(publicKey PublicKey, amountNanos uint64, spendableBalanceNanos uint64) error {
 	pml.RLock()
 	defer pml.RUnlock()
 
-	reservedBalance, exists := pml.reservedBalances[publicKey]
+	reservedBalance, exists := pml.reservedBalancesNanos[publicKey]
 
 	// Check for reserved balance overflow.
-	if exists && amount > math.MaxUint64-reservedBalance {
+	if exists && amountNanos > math.MaxUint64-reservedBalance {
 		return errors.Errorf("CheckBalanceIncrease: Reserved balance overflow")
 	}
 
-	newReservedBalance := reservedBalance + amount
-	spendableBalanceNanos, err := blockView.GetSpendableDeSoBalanceNanosForPublicKey(publicKey.ToBytes(), blockHeight)
-	if err != nil {
-		return errors.Wrapf(err, "CheckBalanceIncrease: Problem getting spendable balance")
-	}
+	newReservedBalance := reservedBalance + amountNanos
 	if newReservedBalance > spendableBalanceNanos {
 		return errors.Errorf("PosMempool.AddTransaction: Not enough balance to cover txn fees "+
 			"(newReservedBalance: %d, spendableBalanceNanos: %d)", newReservedBalance, spendableBalanceNanos)
@@ -41,11 +41,11 @@ func (pml *PosMempoolLedger) CheckBalanceIncrease(publicKey PublicKey, amount ui
 	return nil
 }
 
-func (pml *PosMempoolLedger) CheckBalanceDecrease(publicKey PublicKey, amount uint64) error {
+func (pml *BalanceLedger) CheckBalanceDecrease(publicKey PublicKey, amount uint64) error {
 	pml.RLock()
 	defer pml.RUnlock()
 
-	reservedBalance, exists := pml.reservedBalances[publicKey]
+	reservedBalance, exists := pml.reservedBalancesNanos[publicKey]
 	if !exists {
 		return errors.Errorf("CheckBalanceDecrease: No reserved balance for public key")
 	}
@@ -55,48 +55,48 @@ func (pml *PosMempoolLedger) CheckBalanceDecrease(publicKey PublicKey, amount ui
 	return nil
 }
 
-func (pml *PosMempoolLedger) IncreaseBalance(publicKey PublicKey, amount uint64) {
+func (pml *BalanceLedger) IncreaseBalance(publicKey PublicKey, amount uint64) {
 	pml.Lock()
 	defer pml.Unlock()
 
-	reservedBalance, exists := pml.reservedBalances[publicKey]
+	reservedBalance, exists := pml.reservedBalancesNanos[publicKey]
 	if !exists {
-		pml.reservedBalances[publicKey] = amount
+		pml.reservedBalancesNanos[publicKey] = amount
 		return
 	}
-	pml.reservedBalances[publicKey] = reservedBalance + amount
+	pml.reservedBalancesNanos[publicKey] = reservedBalance + amount
 }
 
-func (pml *PosMempoolLedger) DecreaseBalance(publicKey PublicKey, amount uint64) {
+func (pml *BalanceLedger) DecreaseBalance(publicKey PublicKey, amount uint64) {
 	pml.Lock()
 	defer pml.Unlock()
 
-	reservedBalance, exists := pml.reservedBalances[publicKey]
+	reservedBalance, exists := pml.reservedBalancesNanos[publicKey]
 	if !exists {
 		return
 	}
 	if amount > reservedBalance {
-		pml.reservedBalances[publicKey] = 0
+		pml.reservedBalancesNanos[publicKey] = 0
 		return
 	}
 
-	pml.reservedBalances[publicKey] = reservedBalance - amount
+	pml.reservedBalancesNanos[publicKey] = reservedBalance - amount
 }
 
-func (pml *PosMempoolLedger) GetReservedBalance(publicKey PublicKey) uint64 {
+func (pml *BalanceLedger) GetReservedBalance(publicKey PublicKey) uint64 {
 	pml.RLock()
 	defer pml.RUnlock()
 
-	reservedBalance, exists := pml.reservedBalances[publicKey]
+	reservedBalance, exists := pml.reservedBalancesNanos[publicKey]
 	if !exists {
 		return 0
 	}
 	return reservedBalance
 }
 
-func (pml *PosMempoolLedger) Reset() {
+func (pml *BalanceLedger) Reset() {
 	pml.Lock()
 	defer pml.Unlock()
 
-	pml.reservedBalances = make(map[PublicKey]uint64)
+	pml.reservedBalancesNanos = make(map[PublicKey]uint64)
 }
