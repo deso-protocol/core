1d0ea29d32f257d221ac51c17a200f81389a4917
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2023-07-25 10:35:37 -0700

    PoS Mempool Balance Ledger (#599)
    * Revert "Split transaction register changes"
    
    This reverts commit 4798856f05e8a23098269f67d6d26077fed8b8d1.
    
    * Split Balance Ledger
    
    * Renames; Function signature changes
    
    * Comments; Add test
    
    * Balance ledger review
    
    * Nits
    
    * Nit

---
+++ lib/pos_mempool_ledger.go
@@ -0,0 +1,110 @@
+package lib
+
+import (
+	"github.com/pkg/errors"
+	"math"
+	"sync"
+)
+
+// BalanceLedger is a simple in-memory ledger of balances for user public keys. The balances in the ledger can be
+// increased or decreased, as long as user's new balance doesn't exceed the user's total max balance.
+type BalanceLedger struct {
+	sync.RWMutex
+
+	// Map of public keys to balances.
+	balances map[PublicKey]uint64
+}
+
+func NewBalanceLedger() *BalanceLedger {
+	return &BalanceLedger{
+		balances: make(map[PublicKey]uint64),
+	}
+}
+
+// CanIncreaseBalance checks if the user's balance can be increased by the given amount. If the user's balance + amount
+// is less or equal than the provided maxBalance, the increase is allowed. Otherwise, an error is returned.
+func (bl *BalanceLedger) CanIncreaseBalance(publicKey PublicKey, amount uint64, maxBalance uint64) error {
+	bl.RLock()
+	defer bl.RUnlock()
+
+	balance, exists := bl.balances[publicKey]
+
+	// Check for balance overflow.
+	if exists && amount > math.MaxUint64-balance {
+		return errors.Errorf("CanIncreaseBalance: balance overflow")
+	}
+
+	newBalance := balance + amount
+	if newBalance > maxBalance {
+		return errors.Errorf("CanIncreaseBalance: Not enough balance to cover txn fees "+
+			"(newBalance: %d, maxBalance: %d)", newBalance, maxBalance)
+	}
+	return nil
+}
+
+// CanDecreaseBalance checks if the user's balance can be decreased by the given amount. If the user's balance is
+// greater or equal to the amount, the decrease is allowed. Otherwise, an error is returned.
+func (bl *BalanceLedger) CanDecreaseBalance(publicKey PublicKey, amountNanos uint64) error {
+	bl.RLock()
+	defer bl.RUnlock()
+
+	balance, exists := bl.balances[publicKey]
+	if !exists {
+		return errors.Errorf("CanDecreaseBalance: No balance for public key")
+	}
+	if amountNanos > balance {
+		return errors.Errorf("CanDecreaseBalance: Amount exceeds current balance")
+	}
+	return nil
+}
+
+// IncreaseBalance increases the user's balance by the given amount. CanIncreaseBalance should be called before
+// calling this function to ensure the increase is allowed.
+func (bl *BalanceLedger) IncreaseBalance(publicKey PublicKey, amount uint64) {
+	bl.Lock()
+	defer bl.Unlock()
+
+	balance, _ := bl.balances[publicKey]
+	// Check for balance overflow.
+	if amount > math.MaxUint64-balance {
+		bl.balances[publicKey] = math.MaxUint64
+		return
+	}
+
+	bl.balances[publicKey] = balance + amount
+}
+
+// DecreaseBalance decreases the user's balance by the given amount. CanDecreaseBalance should be called before
+// calling this function to ensure the decrease is allowed.
+func (bl *BalanceLedger) DecreaseBalance(publicKey PublicKey, amount uint64) {
+	bl.Lock()
+	defer bl.Unlock()
+
+	balance, exists := bl.balances[publicKey]
+	if !exists {
+		return
+	}
+	// Check for balance underflow.
+	if amount > balance {
+		delete(bl.balances, publicKey)
+		return
+	}
+
+	bl.balances[publicKey] = balance - amount
+}
+
+// GetBalance returns the user's balance.
+func (bl *BalanceLedger) GetBalance(publicKey PublicKey) uint64 {
+	bl.RLock()
+	defer bl.RUnlock()
+
+	balance, _ := bl.balances[publicKey]
+	return balance
+}
+
+func (bl *BalanceLedger) Reset() {
+	bl.Lock()
+	defer bl.Unlock()
+
+	bl.balances = make(map[PublicKey]uint64)
+}
---
+++ lib/pos_mempool_ledger_test.go
@@ -0,0 +1,52 @@
+package lib
+
+import (
+	"github.com/stretchr/testify/require"
+	"math"
+	"testing"
+)
+
+func TestBalanceLedger(t *testing.T) {
+	require := require.New(t)
+
+	pk0 := *NewPublicKey(m0PkBytes)
+	pk1 := *NewPublicKey(m1PkBytes)
+	pk2 := *NewPublicKey(m2PkBytes)
+
+	// Sanity-check some balance increase and decreases for pk0
+	balanceLedger := NewBalanceLedger()
+	require.NoError(balanceLedger.CanIncreaseBalance(pk0, 100, 100))
+	require.NoError(balanceLedger.CanIncreaseBalance(pk0, 0, 100))
+	balanceLedger.IncreaseBalance(pk0, 100)
+	require.Equal(uint64(100), balanceLedger.GetBalance(pk0))
+	require.NoError(balanceLedger.CanIncreaseBalance(pk0, 0, 100))
+	require.Error(balanceLedger.CanIncreaseBalance(pk0, 1, 100))
+	require.Error(balanceLedger.CanIncreaseBalance(pk0, 0, 99))
+	require.Error(balanceLedger.CanIncreaseBalance(pk0, math.MaxUint64, math.MaxUint64))
+	require.NoError(balanceLedger.CanDecreaseBalance(pk0, 100))
+	require.NoError(balanceLedger.CanDecreaseBalance(pk0, 0))
+	require.Error(balanceLedger.CanDecreaseBalance(pk0, 101))
+	require.Error(balanceLedger.CanDecreaseBalance(pk0, math.MaxUint64))
+	balanceLedger.DecreaseBalance(pk0, 100)
+	require.Equal(uint64(0), balanceLedger.GetBalance(pk0))
+	balanceLedger.IncreaseBalance(pk0, 10)
+	require.Equal(uint64(10), balanceLedger.GetBalance(pk0))
+	balanceLedger.DecreaseBalance(pk0, 100)
+	require.Equal(uint64(0), balanceLedger.GetBalance(pk0))
+	balanceLedger.IncreaseBalance(pk0, 100)
+
+	// Increase balance for pk1 and pk2 a couple of times
+	balanceLedger.IncreaseBalance(pk1, 100)
+	balanceLedger.IncreaseBalance(pk2, 100)
+	balanceLedger.DecreaseBalance(pk1, 40)
+	balanceLedger.IncreaseBalance(pk2, 40)
+	require.Equal(uint64(100), balanceLedger.GetBalance(pk0))
+	require.Equal(uint64(60), balanceLedger.GetBalance(pk1))
+	require.Equal(uint64(140), balanceLedger.GetBalance(pk2))
+
+	// Test clearing balance ledger
+	balanceLedger.Reset()
+	require.Equal(uint64(0), balanceLedger.GetBalance(pk0))
+	require.Equal(uint64(0), balanceLedger.GetBalance(pk1))
+	require.Equal(uint64(0), balanceLedger.GetBalance(pk2))
+}
