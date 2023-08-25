e315cae502fa87a93fa7405270f448dde3236bc9
iamsofonias <sofonias@deso.org>
2023-08-25 12:52:45 -0400

    Add test for start and stop

--- consensus/consensus_test.go
+++ consensus/consensus_test.go
@@ -4,6 +4,7 @@ package consensus
 
 import (
 	"testing"
+	"time"
 
 	"github.com/deso-protocol/core/bls"
 	"github.com/deso-protocol/core/collections"
@@ -17,7 +18,7 @@ func TestFastHotStuffInitialization(t *testing.T) {
 	// Test initial status for newly constructed instance
 	{
 		fc := NewFastHotStuffConsensus()
-		require.Equal(t, consensusStatusUninitialized, fc.status)
+		require.Equal(t, consensusStatusNotInitialized, fc.status)
 		require.Equal(t, fc.IsInitialized(), false)
 		require.Equal(t, fc.IsRunning(), false)
 		require.NotPanics(t, fc.Stop) // Calling Stop() on an uninitialized instance should be a no-op
@@ -40,17 +41,66 @@ func TestFastHotStuffInitialization(t *testing.T) {
 	// Test Init() function with valid parameters
 	{
 		fc := NewFastHotStuffConsensus()
-		err := fc.Init(1000000000, 100, createDummyBlock(), createDummyValidatorSet())
+		err := fc.Init(100, 101, createDummyBlock(), createDummyValidatorSet())
 		require.NoError(t, err)
 
-		require.Equal(t, consensusStatusNotRunning, fc.status)
+		require.Equal(t, consensusStatusInitialized, fc.status)
 		require.Equal(t, fc.IsInitialized(), true)
 		require.Equal(t, fc.IsRunning(), false)
 
 		require.NotPanics(t, fc.Stop) // Calling Stop() on an initialized instance should be a no-op
 
 		require.Equal(t, fc.chainTip.GetBlockHash().GetValue(), createDummyBlockHash().GetValue())
+		require.Equal(t, fc.chainTip.GetView(), uint64(0))
+		require.Equal(t, fc.chainTip.GetHeight(), uint64(0))
+
+		require.Equal(t, fc.blockConstructionCadence, time.Duration(100))
+		require.Equal(t, fc.timeoutBaseDuration, time.Duration(101))
+
+		require.Equal(t, fc.currentView, uint64(1))
+		require.Equal(t, len(fc.validators), len(createDummyValidatorSet()))
 	}
+}
+
+func TestFastHotStuffEventLoopStartStop(t *testing.T) {
+	oneHourInNanoSecs := time.Duration(3600000000000)
+	tenSecondsInNanoSecs := time.Duration(10000000000)
+
+	fc := NewFastHotStuffConsensus()
+	err := fc.Init(oneHourInNanoSecs, 2*oneHourInNanoSecs, createDummyBlock(), createDummyValidatorSet())
+	require.NoError(t, err)
+
+	go fc.Start()
+
+	// Busy wait until the event loop has started
+	for !fc.IsRunning() {
+		continue
+	}
+
+	// Confirm the consensus instance status has changed to running
+	require.Equal(t, consensusStatusRunning, fc.status)
+
+	// Confirm that the ETAs for the block construction and timeout timers have been set
+	require.Greater(t, fc.nextBlockConstructionTimeStamp, time.Now().Add(
+		oneHourInNanoSecs-tenSecondsInNanoSecs, // Subtract 10 second buffer so this test ins't flaky
+	))
+	require.Greater(t, fc.nextTimeoutTimeStamp, time.Now().Add(
+		2*oneHourInNanoSecs-tenSecondsInNanoSecs, // Subtract 10 second buffer so this test ins't flaky
+	))
+
+	// Stop the event loop
+	fc.Stop()
+
+	// Busy wait until the event loop has stopped
+	for fc.IsRunning() {
+		continue
+	}
+
+	// Confirm the consensus instance status has reverted to initialized
+	require.Equal(t, consensusStatusInitialized, fc.status)
+
+	// Confirm that calling fc.Stop() again doesn't panic
+	require.NotPanics(t, fc.Stop)
 }
 
 func createDummyValidatorSet() []Validator {
