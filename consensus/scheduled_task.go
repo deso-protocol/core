8f396649bc486c83ebe58867f0471ee7ac87b991
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2023-09-12 13:27:10 -0700

    Add status and seq to ScheduledTask

--- consensus/scheduled_task.go
+++ consensus/scheduled_task.go
@@ -1,9 +1,20 @@
 package consensus
 
 import (
+	"github.com/pkg/errors"
 	"sync"
 	"time"
 )
+
+type scheduledTaskStatus int
+
+const (
+	scheduledTaskStatusNotStarted scheduledTaskStatus = iota
+	scheduledTaskStatusStarted
+	scheduledTaskStatusFinished
+)
+
+const errorScheduledTaskNotFinished = "ScheduledTask has started and must finish before a new task can be scheduled."
 
 // ScheduledTask is a thread-safe wrapper around time.Timer that allows for creating tasks that
 // can be scheduled to execute at a later time with pre-specified params. Both the params and the
@@ -13,6 +24,8 @@ import (
 // This pattern is useful for spawning off tasks that we want to run after some specified amount
 // of time, but still want to have the ability to cancel.
 type ScheduledTask[TaskParam any] struct {
+	status   scheduledTaskStatus
+	seq      uint64
 	lock     sync.RWMutex
 	timer    *time.Timer
 	duration time.Duration
@@ -20,16 +33,24 @@ type ScheduledTask[TaskParam any] struct {
 
 func NewScheduledTask[TaskParam any]() *ScheduledTask[TaskParam] {
 	return &ScheduledTask[TaskParam]{
-		lock:  sync.RWMutex{},
-		timer: nil,
+		status: scheduledTaskStatusNotStarted,
+		seq:    0,
+		lock:   sync.RWMutex{},
+		timer:  nil,
 	}
 }
 
 // Schedule a new task to be executed after the countdown duration. If there is an existing scheduled
 // task, it will be cancelled and replaced with the new task.
-func (t *ScheduledTask[TaskParam]) Schedule(duration time.Duration, param TaskParam, task func(TaskParam)) {
+func (t *ScheduledTask[TaskParam]) Schedule(duration time.Duration, param TaskParam, task func(TaskParam)) error {
 	t.lock.Lock()
 	defer t.lock.Unlock()
+
+	if t.status == scheduledTaskStatusStarted {
+		return errors.New(errorScheduledTaskNotFinished)
+	}
+	t.status = scheduledTaskStatusNotStarted
+	t.seq++
 
 	if t.timer != nil {
 		t.timer.Stop()
@@ -40,14 +61,34 @@ func (t *ScheduledTask[TaskParam]) Schedule(duration time.Duration, param TaskPa
 	t.duration = duration
 
 	// Replacing the timer results in it being garbage collected, so this is entirely safe.
+	taskFunc := func(seq uint64) {
+		t.lock.Lock()
+		defer t.lock.Unlock()
+
+		if t.seq != seq {
+			return
+		}
+		t.status = scheduledTaskStatusStarted
+	}
 	t.timer = time.AfterFunc(duration, func() {
+		taskFunc(t.seq)
 		task(param)
+
+		t.lock.Lock()
+		defer t.lock.Unlock()
+		t.status = scheduledTaskStatusFinished
 	})
+
+	return nil
 }
 
 func (t *ScheduledTask[TaskParam]) Cancel() {
 	t.lock.Lock()
 	defer t.lock.Unlock()
+
+	if t.status == scheduledTaskStatusStarted {
+		return
+	}
 
 	if t.timer != nil {
 		t.timer.Stop()
