41779e5c36c481a9614ee69cd4c6136e5062f8ca
Lazy Nina <>
2023-09-28 18:40:41 -0400

    refactor

--- lib/blockchain.go
+++ lib/blockchain.go
@@ -2653,24 +2653,10 @@ func (bc *Blockchain) processBlockPoW(desoBlock *MsgDeSoBlock, verifySignatures 
 func (bc *Blockchain) processBlockPoS(desoBlock *MsgDeSoBlock, verifySignatures bool) (_success bool, _isOrphan bool, _missingBlockHashes []*BlockHash, _err error) {
 	// TODO: Implement me
 	// 1. Start with all sanity checks of the block.
-	// 1a. Surface Level validation fo the block
-	if err := bc.validateBlockGeneral(desoBlock); err != nil {
-		return false, false, nil, err
-	}
-	// 1b. Validate Block Height
-	if err := bc.validateBlockHeight(desoBlock); err != nil {
-		return false, false, nil, err
-	}
-	// 1c. Validate View
-	if err := bc.validateBlockView(desoBlock); err != nil {
-		// Check if err is for view > latest committed block view and <= latest uncommitted block.
-		// If so, we need to perform the rest of the validations and then add to our block index.
-		// TODO: implement check on error described above.
-		return false, false, nil, err
-	}
-	// 1d. Validate Leader
-	if err := bc.validateBlockLeader(desoBlock); err != nil {
-		return false, false, nil, err
+	// TODO: Check if err is for view > latest committed block view and <= latest uncommitted block.
+	// If so, we need to perform the rest of the validations and then add to our block index.
+	if err := bc.validateDeSoBlockPoS(desoBlock); err != nil {
+
 	}
 	// TODO: Get validator set for current block height. Alternatively, we could do this in
 	// validateQC, but we may need the validator set elsewhere in this function anyway.
@@ -2680,6 +2666,7 @@ func (bc *Blockchain) processBlockPoS(desoBlock *MsgDeSoBlock, verifySignatures 
 		return false, false, nil, err
 	}
 
+	// @sofonias @piotr - should we move this to
 	// If the block doesn’t contain a ValidatorsTimeoutAggregateQC, then that indicates that we
 	// did NOT timeout in the previous view, which means we should just check that
 	// the QC corresponds to the previous view.
@@ -2756,57 +2743,6 @@ func (bc *Blockchain) processBlockPoS(desoBlock *MsgDeSoBlock, verifySignatures 
 
 	// 9. Return success
 	return true, false, nil, nil
-}
-
-// validateBlockGeneral validates the block at a surface level. It checks
-// that the timestamp is valid, that the version of the header is valid,
-// and other general integrity checks (such as not malformed).
-func (bc *Blockchain) validateBlockGeneral(desoBlock *MsgDeSoBlock) error {
-	// TODO: Implement me
-	return errors.New("IMPLEMENT ME")
-}
-
-// validateBlockHeight validates the block height for a given block. It checks
-// that this block height is exactly one greater than the current block height.
-// TODO: Are we sure that's the correct validation here?
-func (bc *Blockchain) validateBlockHeight(desoBlock *MsgDeSoBlock) error {
-	// TODO: Implement me
-	return errors.New("IMPLEMENT ME")
-}
-
-// validateBlockView validates the view for a given block. First, it checks that
-// the view is greater than the latest committed block view. If not,
-// we return an error indicating that we'll never accept this block. Next,
-// it checks that the view is less than or equal to the latest uncommitted block.
-// If not, we return an error indicating that we'll want to add this block as an
-// orphan. Then it will check if that the view is exactly one greater than the
-// latest uncommitted block if we have an regular vote QC. If this block has a
-// timeout QC, it will check that the view is at least greater than the latest
-// uncommitted block's view + 1.
-func (bc *Blockchain) validateBlockView(desoBlock *MsgDeSoBlock) error {
-	// TODO: Implement me
-	return errors.New("IMPLEMENT ME")
-}
-
-// validateBlockLeader validates that the proposer is the expected proposer for the
-// block height + view number pair.
-func (bc *Blockchain) validateBlockLeader(desoBlock *MsgDeSoBlock) error {
-	// TODO: Implement me
-	return errors.New("IMPLEMENT ME")
-}
-
-// validateQC validates that the QC of this block is valid, meaning a super majority
-// of the validator set has voted (or timed out). Assumes ValidatorEntry list is sorted.
-func (bc *Blockchain) validateQC(desoBlock *MsgDeSoBlock, validatorSet []*ValidatorEntry) error {
-	// TODO: Implement me
-	return errors.New("IMPLEMENT ME")
-}
-
-// validateTimeoutQC validates that the parent block hash is not too far back from the latest.
-// Specifically, it checks that the parent block hash is at least the latest committed block.
-func (bc *Blockchain) validateTimeoutQC(desoBlock *MsgDeSoBlock, validatorSet []*ValidatorEntry) error {
-	// TODO: Implement me
-	return errors.New("IMPLEMENT ME")
 }
 
 // validateAncestorsExist checks that all ancestors of this block exist in the block index.
