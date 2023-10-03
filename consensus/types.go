430245f0b27e52e68ca0c252f85c3255f006ab26
iamsofonias <sofonias@deso.org>
2023-10-03 14:55:13 -0700

    Remove redundant elapsed flag

--- consensus/types.go
+++ consensus/types.go
@@ -148,8 +148,6 @@ type FastHotStuffEventLoop struct {
 	crankTimerInterval  time.Duration
 	timeoutBaseDuration time.Duration
 
-	hasCrankTimerElapsed bool
-
 	crankTimerTask  *ScheduledTask[uint64]
 	nextTimeoutTask *ScheduledTask[uint64]
 
