75b955c31dd2f77a26ddd506587a975a56d0200a
Lazy Nina <>
2023-10-23 18:57:44 -0400

    only set proposed in view on header v1 since that's all we need to set up for pos

--- lib/network.go
+++ lib/network.go
@@ -2284,8 +2284,6 @@ func DecodeHeaderVersion0(rr io.Reader) (*MsgDeSoHeader, error) {
 			return nil, errors.Wrapf(err, "MsgDeSoHeader.FromBytes: Problem decoding Height")
 		}
 		retHeader.Height = uint64(binary.LittleEndian.Uint32(scratchBytes[:]))
-		// TODO: Is this okay? Feels like it greatly simplifies things...
-		retHeader.ProposedInView = retHeader.Height
 	}
 
 	// Nonce
