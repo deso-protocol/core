dc49488d9da74fd6305e75d03ee88a29cf4d7bc6
tholonious <99746187+tholonious@users.noreply.github.com>
2023-11-01 19:15:56 -0400

    Move basic skeleton for PoS message handling and syncing to separate file (#763)
    * Create basic skeleton for PoS message handling and syncing
    
    * Simplify interface
    
    * Create new struct
    
    * Create constrtuctor for ConsensusMessageHandler
    
    * rename ConsensusMessageHandler to ConsensusController

---
+++ lib/pos_consensus_controller.go
@@ -0,0 +1,77 @@
+package lib
+
+import "github.com/deso-protocol/core/consensus"
+
+type ConsensusController struct {
+	fastHotStuffEventLoop *consensus.FastHotStuffEventLoop
+	blockchain            *Blockchain
+}
+
+func NewConsensusController(
+	fastHotStuffEventLoop *consensus.FastHotStuffEventLoop,
+	blockchain *Blockchain,
+) *ConsensusController {
+	return &ConsensusController{
+		fastHotStuffEventLoop: fastHotStuffEventLoop,
+		blockchain:            blockchain,
+	}
+}
+
+func (cc *ConsensusController) HandleFastHostStuffBlockProposal(event *consensus.FastHotStuffEvent) {
+	// The consensus module has signaled that we can propose a block at a certain block
+	// height. We construct the block and broadcast it here:
+	// 1. Verify that the block height we want to propose at is valid
+	// 2. Get a QC from the consensus module
+	// 3. Iterate over the top n transactions from the mempool
+	// 4. Construct a block with the QC and the top n transactions from the mempool
+	// 5. Sign the block
+	// 6. Process the block locally
+	//   - This will connect the block to the blockchain, remove the transactions from the
+	//   - mempool, and process the vote in the consensus module
+	// 7. Broadcast the block to the network
+}
+
+func (cc *ConsensusController) HandleFastHostStuffEmptyTimeoutBlockProposal(event *consensus.FastHotStuffEvent) {
+	// The consensus module has signaled that we have a timeout QC and can propose one at a certain
+	// block height. We construct an empty block with a timeout QC and broadcast it here:
+	// 1. Verify that the block height and view we want to propose at is valid
+	// 2. Get a timeout QC from the consensus module
+	// 3. Construct a block with the timeout QC
+	// 4. Sign the block
+	// 5. Process the block locally
+	// 6. Broadcast the block to the network
+}
+
+func (cc *ConsensusController) HandleFastHostStuffVote(event *consensus.FastHotStuffEvent) {
+	// The consensus module has signaled that we can vote on a block. We construct and
+	// broadcast the vote here:
+	// 1. Verify that the block height we want to vote on is valid
+	// 2. Construct the vote message
+	// 3. Process the vote in the consensus module
+	// 4. Broadcast the timeout msg to the network
+}
+
+func (handler *ConsensusController) HandleFastHostStuffTimeout(event *consensus.FastHotStuffEvent) {
+	// The consensus module has signaled that we have timed out for a view. We construct and
+	// broadcast the timeout here:
+	// 1. Verify the block height and view we want to timeout on are valid
+	// 2. Construct the timeout message
+	// 3. Process the timeout in the consensus module
+	// 4. Broadcast the timeout msg to the network
+}
+
+func (cc *ConsensusController) HandleHeaderBundle(pp *Peer, msg *MsgDeSoHeaderBundle) {
+	// TODO
+}
+
+func (cc *ConsensusController) HandleGetBlocks(pp *Peer, msg *MsgDeSoGetBlocks) {
+	// TODO
+}
+
+func (cc *ConsensusController) HandleHeader(pp *Peer, msg *MsgDeSoHeader) {
+	// TODO
+}
+
+func (cc *ConsensusController) HandleBlock(pp *Peer, msg *MsgDeSoBlock) {
+	// TODO
+}
--- lib/server.go
+++ lib/server.go
@@ -61,6 +61,7 @@ type Server struct {
 	eventManager  *EventManager
 	TxIndex       *TXIndex
 
+	consensusController   *ConsensusController
 	fastHotStuffEventLoop *consensus.FastHotStuffEventLoop
 	// posMempool *PosMemPool TODO: Add the mempool later
 
@@ -2216,59 +2217,23 @@ func (srv *Server) _handlePeerMessages(serverMessage *ServerMessage) {
 	}
 }
 
-func (srv *Server) _handleFastHostStuffBlockProposal(event *consensus.FastHotStuffEvent) {
-	// The consensus module has signaled that we can propose a block at a certain block
-	// height. We construct the block and broadcast it here:
-	// 1. Verify that the block height we want to propose at is valid
-	// 2. Get a QC from the consensus module
-	// 3. Iterate over the top n transactions from the mempool
-	// 4. Construct a block with the QC and the top n transactions from the mempool
-	// 5. Sign the block
-	// 6. Process the block locally
-	//   - This will connect the block to the blockchain, remove the transactions from the
-	//   - mempool, and process the vote in the consensus module
-	// 7. Broadcast the block to the network
-}
-
-func (srv *Server) _handleFastHostStuffEmptyTimeoutBlockProposal(event *consensus.FastHotStuffEvent) {
-	// The consensus module has signaled that we have a timeout QC and can propose one at a certain
-	// block height. We construct an empty block with a timeout QC and broadcast it here:
-	// 1. Verify that the block height and view we want to propose at is valid
-	// 2. Get a timeout QC from the consensus module
-	// 3. Construct a block with the timeout QC
-	// 4. Sign the block
-	// 5. Process the block locally
-	// 6. Broadcast the block to the network
-}
-
-func (srv *Server) _handleFastHostStuffVote(event *consensus.FastHotStuffEvent) {
-	// The consensus module has signaled that we can vote on a block. We construct and
-	// broadcast the vote here:
-	// 1. Verify that the block height we want to vote on is valid
-	// 2. Construct the vote message
-	// 3. Process the vote in the consensus module
-	// 4. Broadcast the timeout msg to the network
-}
-
-func (srv *Server) _handleFastHostStuffTimeout(event *consensus.FastHotStuffEvent) {
-	// The consensus module has signaled that we have timed out for a view. We construct and
-	// broadcast the timeout here:
-	// 1. Verify the block height and view we want to timeout on are valid
-	// 2. Construct the timeout message
-	// 3. Process the timeout in the consensus module
-	// 4. Broadcast the timeout msg to the network
-}
-
 func (srv *Server) _handleFastHostStuffConsensusEvent(event *consensus.FastHotStuffEvent) {
+	// This should never happen. If the consensus message handler isn't defined, then something went
+	// wrong during the node initialization. We log it and return early to avoid panicking.
+	if srv.consensusController == nil {
+		glog.Errorf("Server._handleFastHostStuffConsensusEvent: Consensus message handler is nil")
+		return
+	}
+
 	switch event.EventType {
 	case consensus.FastHotStuffEventTypeVote:
-		srv._handleFastHostStuffVote(event)
+		srv.consensusController.HandleFastHostStuffVote(event)
 	case consensus.FastHotStuffEventTypeTimeout:
-		srv._handleFastHostStuffTimeout(event)
+		srv.consensusController.HandleFastHostStuffTimeout(event)
 	case consensus.FastHotStuffEventTypeConstructVoteQC:
-		srv._handleFastHostStuffBlockProposal(event)
+		srv.consensusController.HandleFastHostStuffBlockProposal(event)
 	case consensus.FastHotStuffEventTypeConstructTimeoutQC:
-		srv._handleFastHostStuffEmptyTimeoutBlockProposal(event)
+		srv.consensusController.HandleFastHostStuffEmptyTimeoutBlockProposal(event)
 	}
 }
 
