f40a16487efd64b5502bfa464794dc75273d9e37
Lazy Nina <>
2023-12-05 11:32:49 -0500

    PoS Mempool support for VerifyAndBroadcastTransaction

--- lib/server.go
+++ lib/server.go
@@ -262,6 +262,14 @@ func (srv *Server) BroadcastTransaction(txn *MsgDeSoTxn) ([]*MempoolTx, error) {
 func (srv *Server) VerifyAndBroadcastTransaction(txn *MsgDeSoTxn) error {
 	// Grab the block tip and use it as the height for validation.
 	blockHeight := srv.blockchain.BlockTip().Height
+	if blockHeight >= srv.blockchain.params.ForkHeights.ProofOfStake2ConsensusCutoverBlockHeight {
+		mtxn := NewMempoolTransaction(txn, uint64(time.Now().UnixMicro()))
+		// AddTransaction also performs validations.
+		if err := srv.posMempool.AddTransaction(mtxn, true /*verifySignatures*/); err != nil {
+			return errors.Wrapf(err, "VerifyAndBroadcastTransaction: problem adding txn to pos mempool")
+		}
+		return nil
+	}
 	err := srv.blockchain.ValidateTransaction(
 		txn,
 		// blockHeight is set to the next block since that's where this
