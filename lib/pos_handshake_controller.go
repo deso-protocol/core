ee69fc8d660cecede2199a217c2678bd1bf604c9
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2023-12-19 15:58:31 -0800

    Add HandshakeController
    PoS Block Producer: TxnConnectStatusByIndex (#672)
    
    * TransactionConnectStatus and ConnectFailingTransaction
    
    * Revert "Merge branch 'p/bmf-status-connected' into p/failing-transactions"
    
    This reverts commit d3e543c4c3e6f03cc74087b05c268d4449ba1689, reversing
    changes made to 960001ce00bbc1c99afb6ca6f697748bd6d944f6.
    
    * Revert "Revert "Merge branch 'p/bmf-status-connected' into p/failing-transactions""
    
    This reverts commit 10a147654c5147c28ec674d0650bb54c8d9cebce.
    
    * Revert "Merge branch 'p/bmf-status-connected' into p/failing-transactions"
    
    This reverts commit d3e543c4c3e6f03cc74087b05c268d4449ba1689, reversing
    changes made to a9f782751b11e8aa3a0977fd2b2535b1ef4d7242.
    
    * TransactionConnectStatus and ConnectFailingTransaction
    
    * Initial _connectFailingTransaction
    
    * ConnectFailingTransaction and GlobalParamsEntry updates
    
    * Fix merge conflicts
    
    * gofmt
    
    * Fix merge conflicts
    
    * Fix blockheight
    
    * Fix merge conflicts
    
    * gofmt
    
    * Revert connect failing transaction
    
    * Add TxnStatusConnectedIndex to block and header
    
    * Fix naming
    
    * Fix tests; remove asserts
    
    * Update comment
    
    Integration testing updates
    
    PoS Block Producer: TxnConnectStatusByIndex (#672)
    
    * TransactionConnectStatus and ConnectFailingTransaction
    
    * Revert "Merge branch 'p/bmf-status-connected' into p/failing-transactions"
    
    This reverts commit d3e543c4c3e6f03cc74087b05c268d4449ba1689, reversing
    changes made to 960001ce00bbc1c99afb6ca6f697748bd6d944f6.
    
    * Revert "Revert "Merge branch 'p/bmf-status-connected' into p/failing-transactions""
    
    This reverts commit 10a147654c5147c28ec674d0650bb54c8d9cebce.
    
    * Revert "Merge branch 'p/bmf-status-connected' into p/failing-transactions"
    
    This reverts commit d3e543c4c3e6f03cc74087b05c268d4449ba1689, reversing
    changes made to a9f782751b11e8aa3a0977fd2b2535b1ef4d7242.
    
    * TransactionConnectStatus and ConnectFailingTransaction
    
    * Initial _connectFailingTransaction
    
    * ConnectFailingTransaction and GlobalParamsEntry updates
    
    * Fix merge conflicts
    
    * gofmt
    
    * Fix merge conflicts
    
    * Fix blockheight
    
    * Fix merge conflicts
    
    * gofmt
    
    * Revert connect failing transaction
    
    * Add TxnStatusConnectedIndex to block and header
    
    * Fix naming
    
    * Fix tests; remove asserts
    
    * Update comment
    
    RemoteNode and RemoteNodeId
    
    Initial remote node manager tests
    
    remote node tests

---
+++ lib/pos_handshake_controller.go
@@ -0,0 +1,162 @@
+package lib
+
+import (
+	"github.com/decred/dcrd/lru"
+	"github.com/deso-protocol/core/bls"
+	"github.com/golang/glog"
+	"math"
+)
+
+// TODO: Replace this
+func getActiveValidators() map[bls.PublicKey]*ValidatorEntry {
+	// TODO: replace with a getter to retrieve all active validators.
+	activeValidators := []*ValidatorEntry{}
+	allValidatorsMap := make(map[bls.PublicKey]*ValidatorEntry)
+	for _, validator := range activeValidators {
+		pk := validator.VotingPublicKey
+		if pk == nil {
+			continue
+		}
+		allValidatorsMap[*pk] = validator
+	}
+
+	return allValidatorsMap
+}
+
+type HandshakeController struct {
+	rnManager              *RemoteNodeManager
+	usedNonces             lru.Cache
+	protocolOnProofOfStake func() bool                              // TODO
+	getActiveValidators    func() map[bls.PublicKey]*ValidatorEntry // TODO
+}
+
+func NewHandshakeController(rnManager *RemoteNodeManager) *HandshakeController {
+
+	vm := &HandshakeController{
+		rnManager:              rnManager,
+		usedNonces:             lru.NewCache(1000),
+		protocolOnProofOfStake: func() bool { return false },
+	}
+
+	return vm
+}
+
+func (hc *HandshakeController) InitiateHandshake(rn *RemoteNode) {
+
+	nonce := uint64(RandInt64(math.MaxInt64))
+	if err := rn.InitiateHandshake(nonce); err != nil {
+		glog.Errorf("RemoteNode.InitiateHandshake: Error initiating handshake: %v", err)
+		hc.rnManager.Disconnect(rn)
+	}
+}
+
+func (hc *HandshakeController) handlePoSHandshakePeerMessage(remoteNode *RemoteNode) {
+	if !hc.protocolOnProofOfStake() {
+		return
+	}
+
+	// Make sure the peer is on the right proof of stake version
+	if remoteNode.GetNegotiatedProtocolVersion() != ProtocolVersion2 {
+		// Disconnect the peer because we only accept validators running proof of stake.
+		hc.rnManager.Disconnect(remoteNode)
+		return
+	}
+
+	// Get all active validators and see if the peer is one of them.
+	activeValidators := hc.getActiveValidators()
+	validatorPk := remoteNode.GetValidatorPublicKey()
+	if validatorPk == nil {
+		// Disconnect the peer because we only accept validators running proof of stake.
+		hc.rnManager.Disconnect(remoteNode)
+		return
+	}
+
+	// If there's already a validator connected with the same public key, disconnect the peer.
+	if _, ok := hc.rnManager.GetRemoteNodeIndexer().GetValidatorIndex().Get(*validatorPk); ok {
+		hc.rnManager.Disconnect(remoteNode)
+		return
+	}
+
+	// If the peer is not an active validator, there is nothing else to check so return.
+	if _, ok := activeValidators[*validatorPk]; !ok {
+		return
+	}
+}
+
+func (hc *HandshakeController) _handleHandshakePeerMessage(origin *Peer, desoMsg DeSoMessage) {
+	if desoMsg.GetMsgType() != MsgTypePeerHandshakeComplete {
+		return
+	}
+
+	// Get the handshake information of this peer.
+	remoteNode := hc.rnManager.GetRemoteNodeFromPeer(origin)
+	if remoteNode == nil {
+		return
+	}
+
+	if hc.protocolOnProofOfStake() {
+		hc.handlePoSHandshakePeerMessage(remoteNode)
+	}
+	hc.rnManager.ProcessCompletedHandshake(remoteNode)
+}
+
+func (hc *HandshakeController) _handleVersionMessage(origin *Peer, desoMsg DeSoMessage) {
+	if desoMsg.GetMsgType() != MsgTypeVersion {
+		return
+	}
+
+	rn := hc.rnManager.GetRemoteNodeFromPeer(origin)
+	if rn == nil {
+		return
+	}
+
+	var verMsg *MsgDeSoVersion
+	var ok bool
+	if verMsg, ok = desoMsg.(*MsgDeSoVersion); !ok {
+		hc.rnManager.Disconnect(rn)
+		return
+	}
+
+	// If we've sent this nonce before then return an error since this is
+	// a connection from ourselves.
+	msgNonce := verMsg.Nonce
+	if hc.usedNonces.Contains(msgNonce) {
+		hc.usedNonces.Delete(msgNonce)
+		glog.V(1).Infof("HandshakeController._handleVersionMessage: Requesting PeerDisconnect for id: (%v) "+
+			"nonce collision", origin.ID)
+		rn.Disconnect()
+		return
+	}
+
+	responseNonce := uint64(RandInt64(math.MaxInt64))
+	rn.HandleVersionMessage(verMsg, responseNonce)
+	hc.usedNonces.Add(responseNonce)
+}
+
+func (hc *HandshakeController) _handleVerackMessage(origin *Peer, desoMsg DeSoMessage) {
+	if desoMsg.GetMsgType() != MsgTypeVerack {
+		return
+	}
+
+	rn := hc.rnManager.GetRemoteNodeFromPeer(origin)
+	if rn == nil {
+		return
+	}
+
+	var vrkMsg *MsgDeSoVerack
+	var ok bool
+	if vrkMsg, ok = desoMsg.(*MsgDeSoVerack); !ok {
+		hc.rnManager.Disconnect(rn)
+		return
+	}
+
+	if !ok {
+		glog.V(1).Infof("HandshakeController._handleVerackMessage: Requesting PeerDisconnect for id: (%v) "+
+			"nonce not found for peer", origin.ID)
+		rn.Disconnect()
+		return
+	}
+
+	rn.HandleVerackMessage(vrkMsg)
+	return
+}
