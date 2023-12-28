98551c14fed00d9bf922027e927797792daf3019
Lazy Nina <81658138+lazynina@users.noreply.github.com>
2023-12-28 13:22:37 -0500

    Don't add to the queue in snapshot.Run for the SnapshotOperationProcessChunk, we already added to the channel GetSnapshot (#890)
    * Don't add to the queue in snapshot.Run for the SnapshotOperationProcessChunk, we already added to the channel GetSnapshot
    
    * Don't add to the queue in snapshot.Run for the SnapshotOperationProcessChunk, we already added to the channel GetSnapshot
    
    * trigger build
    
    * fix condition on state change syncer reset
    
    ---------
    
    Co-authored-by: Lazy Nina <>

--- lib/server.go
+++ lib/server.go
@@ -565,7 +565,9 @@ func NewServer(
 	// This can happen if the node was terminated mid-operation last time it was running. The recovery process rolls back
 	// blocks to the beginning of the current snapshot epoch and resets to the state checksum to the epoch checksum.
 	if shouldRestart {
-		stateChangeSyncer.Reset()
+		if stateChangeSyncer != nil {
+			stateChangeSyncer.Reset()
+		}
 		glog.Errorf(CLog(Red, "NewServer: Forcing a rollback to the last snapshot epoch because node was not closed "+
 			"properly last time"))
 		if err := _snapshot.ForceResetToLastSnapshot(_chain); err != nil {
