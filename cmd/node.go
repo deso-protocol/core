b9f4a3935241a04d7f83c451c2d060a1491138e0
superzordon <zordonclout@gmail.com>
2024-01-17 00:05:15 -0500

    Update documentation around bugfix

--- cmd/node.go
+++ cmd/node.go
@@ -157,6 +157,11 @@ func (node *Node) Start(exitChannels ...*chan struct{}) {
 	// Check to see if this node has already been initialized with performance or default options.
 	// If so, we should continue to use those options.
 	// If not, it should be based on the sync type.
+	// The reason we do this check is because once a badger database is initialized with performance options,
+	// re-opening it with non-performance options results in a memory error panic. In order to prevent this transition
+	// from default -> performance -> default settings, we save the db options to a file. This takes the form of a
+	// boolean which indicates whether the db was initialized with performance options or not. Upon restart, if the
+	// file exists, we use the same options. If the file does not exist, we use the options based on the sync type.
 	performanceOptions, err := lib.DbInitializedWithPerformanceOptions(node.Config.DataDirectory)
 
 	// If the db options haven't yet been saved, we should base the options on the sync type.
