794fc1a819161ff4342a2df973d70807c414048a
iamsofonias <sofonias@deso.org>
2024-01-25 11:01:26 -0500

    Pass PoS Mempool Init Params From Command Line Flags

--- cmd/node.go
+++ cmd/node.go
@@ -253,6 +253,8 @@ func (node *Node) Start(exitChannels ...*chan struct{}) {
 		node.Config.StateChangeDir,
 		node.Config.HypersyncMaxQueueSize,
 		blsKeystore,
+		node.Config.MaxMempoolPosSizeBytes,
+		node.Config.MempoolBackupIntervalMillis,
 	)
 	if err != nil {
 		// shouldRestart can be true if, on the previous run, we did not finish flushing all ancestral
