04205db0c1dddbbe5d1001036a93dfb24c861571
Piotr Nojszewski <29924594+AeonSw4n@users.noreply.github.com>
2024-01-30 12:18:58 -0800

    Fix RegtestMiner

--- integration_testing/mining_test.go
+++ integration_testing/mining_test.go
@@ -6,12 +6,11 @@ import (
 )
 
 // TestSimpleBlockSync test if a node can mine blocks on regtest
-// TODO: This test doesn't work, we should investigate why.
 func TestRegtestMiner(t *testing.T) {
-	t.Skipf("TestRegtestMiner doesn't work in PoS")
-
 	node1 := spawnNodeProtocol1(t, 18000, "node1")
-	node1.Config.Params = &lib.DeSoTestnetParams
+	params := lib.DeSoTestnetParams
+	node1.Config.Params = &params
+	node1.Params = &params
 	node1.Config.MaxSyncBlockHeight = 0
 	node1.Config.MinerPublicKeys = []string{"tBCKVERmG9nZpHTk2AVPqknWc1Mw9HHAnqrTpW1RnXpXMQ4PsQgnmV"}
 	node1.Config.Regtest = true
