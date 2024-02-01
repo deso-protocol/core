043991251c372574dd5fff9a5ffc264322a12165
tholonious <99746187+tholonious@users.noreply.github.com>
2024-02-01 17:50:35 -0500

    Attach Listeners To Register Node As Validator on Testnet (#998)

---
+++ lib/pos_server_regtest.go
@@ -0,0 +1,13 @@
+package lib
+
+func (srv *Server) submitRegtestRegisterAsValidatorTxn(block *MsgDeSoBlock) {
+
+}
+
+func (srv *Server) submitRegtestStakeTxn(block *MsgDeSoBlock) {
+
+}
+
+func (srv *Server) startRegtestFastHotStuffConsensus(block *MsgDeSoBlock) {
+
+}
